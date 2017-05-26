f2056eb16b02c36d4cad718f48ea9c98b1aa22ae
Lisa Xu <lisa.yaqing.xu@gmail.com>
2017-05-26 00:36:37 -0400

    refactored code with better best practices and removed bootstrap

--- js/script.js
+++ js/script.js
@@ -1,64 +1,65 @@
-+function(){
+(function(){
+	'use strict'
 	//fields
-	var canvas = $("#dotcanvas")[0]
-	var offset = $(canvas).offset();
-	var ctx = canvas.getContext("2d");
-	var canvaswidth = canvas.width;
-	var canvasheight = canvas.height;
-	var pi = 3.1415926535897;
-	var pts = [];
-	var _LEFT = 1;
-	var _RIGHT = -1;
-	var _SAME = 0;
+	const canvas = $("#dotcanvas")[0] 
+	const canvasOffset = $(canvas).offset();
+	const ctx = canvas.getContext("2d");
+	const canvasWidth = canvas.width;
+	const canvasHeight = canvas.height;
+	const LEFT = 1;
+	const RIGHT = -1;
+	const SAME = 0;
+
+	let pts = [];
 	//ui functions
-	var drawdot = function(dot){
+	function renderPoint(point){
 		ctx.beginPath();
-		ctx.arc(dot.x,dot.y,2,0,2*Math.PI);
+		ctx.arc(point.x,point.y,2,0,2*Math.PI);
 		ctx.fill();
 	}
-	var clearCanvas = function(){
-		ctx.clearRect(0, 0, canvaswidth, canvasheight);
+	function clearCanvas(){
+		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 	}
-	var bot_limit = 500;
-	var top_limit = 500;
+	let minNumPoints = 500;
+	let maxNumPoints = 1000;
 	//randomly generate points
-	var generatedots = function(){
+	function generatePoints(){
 		clearCanvas();
 		pts = [];
 		
-		var numpts = Math.floor(Math.random()*(top_limit-bot_limit+1))+bot_limit;
-		for(var i = 0; i < numpts; i++){
-			var genx = Math.floor(Math.random()*(canvaswidth-60))+30;
-			var geny = Math.floor(Math.random()*(canvasheight-60))+30;
-			var dot = {x:genx,y:geny};
-			drawdot(dot);
+		let numpts = Math.floor(Math.random()*(maxNumPoints-minNumPoints+1))+minNumPoints;
+		for(let i = 0; i < numpts; i++){
+			let genx = Math.floor(Math.random()*(canvasWidth-60))+30;
+			let geny = Math.floor(Math.random()*(canvasHeight-60))+30;
+			let dot = {x:genx,y:geny};
+			renderPoint(dot);
 			pts.push(dot);
 		}
 		//console.log(pts);
 
 	}
-	var redrawdots = function(){
+	function redrawPoints(points){
 		clearCanvas();
-		for(var i = 0; i < pts.length;i++){
-			drawdot(pts[i]);
+		for(let i = 0; i < points.length;i++){
+			renderPoint(points[i]);
 		}
 	}
-	var linepts = function(p){
+	function connectPointsByLine(points){
 		ctx.beginPath();
-		ctx.moveTo(p[0].x,p[0].y);
+		ctx.moveTo(points[0].x,points[0].y);
 		//console.log(p.length);
 		//console.log(p);
-		for(var i = 1; i < p.length; i ++){
+		for(let i = 1; i < points.length; i ++){
 			//console.log(i);
-			ctx.lineTo(p[i].x,p[i].y);
+			ctx.lineTo(points[i].x,points[i].y);
 			
 		}
-		ctx.lineTo(p[0].x,p[0].y);
+		ctx.lineTo(points[0].x,points[0].y);
 		ctx.stroke();
 		ctx.closePath();
 	}
 
-	var drawline = function(p1,p2){
+	function renderLine(p1,p2){
 		ctx.beginPath();
 		ctx.moveTo(p1.x,p1.y);
 		ctx.lineTo(p2.x,p2.y);
@@ -67,94 +68,107 @@
 
 	}
 
-	//angle calculation helper function
-	var angletopt = function(pt1, pt2){
-		/*if(pt1.x === pt2.x) return pi/2; //same x case
-		if(pt1.y === pt2.y){
-			if(pt1.x > pt2.x) return pi; //180 degrees
-			else return 0; 
-		}*/
-		var num = Math.atan2(pt1.y-pt2.y,pt2.x-pt1.x);
-		//console.log(num)
-		return num;
+	/**
+	 * Calculate the angle between two points with respect to the x axis
+	 * @param {*} pt1 
+	 * @param {*} pt2 
+	 */
+	function angleToPoint(pt1, pt2){
+		return Math.atan2(pt1.y-pt2.y,pt2.x-pt1.x);
+		
 	}
-	//find angle between 3 points, 
-	//pt2 is the center point
-	var angle3pt = function(pt1,pt2,pt3){
-		var ab = Math.sqrt(Math.pow(pt2.x-pt1.x,2)+ Math.pow(pt2.y-pt1.y,2));    
-    	var bc = Math.sqrt(Math.pow(pt2.x-pt3.x,2)+ Math.pow(pt2.y-pt3.y,2)); 
-    	var ac = Math.sqrt(Math.pow(pt3.x-pt1.x,2)+ Math.pow(pt3.y-pt1.y,2));
+	
+	/**
+	 * Calculate the angle between 3 points, with pt2 being the center point
+	 * @param {*} pt1 
+	 * @param {*} pt2 
+	 * @param {*} pt3 
+	 */
+	function getAngleBetween3Points(pt1,pt2,pt3){
+		let ab = Math.sqrt(Math.pow(pt2.x-pt1.x,2)+ Math.pow(pt2.y-pt1.y,2));    
+    	let bc = Math.sqrt(Math.pow(pt2.x-pt3.x,2)+ Math.pow(pt2.y-pt3.y,2)); 
+    	let ac = Math.sqrt(Math.pow(pt3.x-pt1.x,2)+ Math.pow(pt3.y-pt1.y,2));
     	return Math.acos((bc*bc+ab*ab-ac*ac)/(2*bc*ab));
 	}
-	//area2 from the textbook
-	var area2 = function(pt1, pt2, pt3){
+	/**
+	 * based on the textbook, 2d area calculation between 3 points
+	 * @param {*} pt1 
+	 * @param {*} pt2 
+	 * @param {*} pt3 
+	 */
+	function getAreaBetween3Points(pt1, pt2, pt3){
 		//reversed the y because js coordinates
-		return (pt2.x*10 - pt1.x*10)*(pt1.y*10-pt3.y*10)-(pt3.x*10-pt1.x*10)*(pt1.y*10-pt2.y*10);
+		return (pt2.x - pt1.x)*(pt1.y-pt3.y)-(pt3.x-pt1.x)*(pt1.y-pt2.y);
 	}
-	//left turn
-	var left = function(pt1, pt2, pt3){
-		return  area2(pt1,pt2,pt3) > 0; 
+	/**
+	 * 3 points are defined as being "left" if the area between them using the area calculation is positive.
+	 * @param {*} pt1 
+	 * @param {*} pt2 
+	 * @param {*} pt3 
+	 */
+	function checkIfLeftTurn(pt1, pt2, pt3){
+		return  getAreaBetween3Points(pt1,pt2,pt3) > 0; 
 	}
-	var samept = function(p1, p2){
+	function isSamePoint(p1, p2){
 		return p1.x === p2.x && p1.y === p2.y;
 	}
 	//graham scan function
-	var grahamscan = function(p){
-		var v_lowest = p[0];
+	function grahamScan(points){
+		let v_lowest = points[0];
 		//find lowest point
-		for(var i = 1; i < p.length; i++){
+		for(let i = 1; i < points.length; i++){
 			//console.log(p[i])
-			if(p[i].y > v_lowest.y){
-				v_lowest = p[i];
+			if(points[i].y > v_lowest.y){
+				v_lowest = points[i];
 			}
-			else if(p[i].y === v_lowest.y && p[i].x < v_lowest.x){
-				v_lowest = p[i];
+			else if(points[i].y === v_lowest.y && points[i].x < v_lowest.x){
+				v_lowest = points[i];
 			}
 			//console.log(v_lowest);
 		}
 		if(v_lowest)
 		
 		//sort the array by angle
-		p.sort(function(a,b){
+		points.sort(function(a,b){
 			//a is lowest point
 			if(a.y === v_lowest.y && a.x === v_lowest.x)return -1;
 			//b is lowest point
 			if(b.y === v_lowest.y && b.x === v_lowest.x)return 1;
 			//neither
 			
-			var ang_a = angletopt(v_lowest,a);
-			var ang_b = angletopt(v_lowest,b);
+			let ang_a = angleToPoint(v_lowest,a);
+			let ang_b = angleToPoint(v_lowest,b);
 			if(ang_a > ang_b) return 1;
 			else return -1;
 		});
 
 		//remove doubles
-		var i = 0;
-		while(i < p.length-1){
-			if(p[i].x === p[i+1].x && p[i].y === p[i+1].y){
-				p.splice(i+1,0);
+		let i = 0;
+		while(i < points.length-1){
+			if(points[i].x === points[i+1].x && points[i].y === points[i+1].y){
+				points.splice(i+1,0);
 			}
 			i++;
 		}
 
-		var stack = [];
-		if(p.length < 4) return p;
+		let stack = [];
+		if(points.length < 4) return points;
 		 //yeah im hardcoding this but we're not doing 
 		 //graham scan with < 4 things anyway i think having
 		 //2 is ok
 
-		stack[0] = p[0];
-		stack[1] = p[1];
-		var current = p[2];
-		var index = 2;
-		var stacklen = 2;
-		while(index < p.length){
+		stack[0] = points[0];
+		stack[1] = points[1];
+		let current = points[2];
+		let index = 2;
+		let stacklen = 2;
+		while(index < points.length){
 			stacklen = stack.length;
 			//console.log(stacklen);
 			if(stacklen > 1){//make sure there's at least 2 things before left test
-				var l = left(stack[stacklen-2],stack[stacklen-1],p[index])
+				let l = checkIfLeftTurn(stack[stacklen-2],stack[stacklen-1],points[index])
 				if(l){
-					stack.push(p[index]);
+					stack.push(points[index]);
 					index++;
 				}
 				else{
@@ -162,47 +176,45 @@
 				}
 			}
 			else{
-				stack.push(p[index]);
+				stack.push(points[index]);
 				index++;
 			}
 			
 		}
-		//stack.push(p[0]);
-		//console.log(stack);
-		//linepts(stack);
+
 		return stack;
 
 	}
 	//variation on binary search, more details in report
-	var tangent_bin_search = function(hull,p1,p2){
+	function tangentBinarySearch(hull,p1,p2){
 		
-		var index = -1;
-		var value = -999;
-		var length = hull.length;
+		let index = -1;
+		let value = -999;
+		let length = hull.length;
 
-		var start = 0;
-		var end = length-1;
-		var leftsplit = -1; 
-		var rightsplit = -1;
+		let start = 0;
+		let end = length-1;
+		let leftsplit = -1; 
+		let rightsplit = -1;
 
-		var direction = null;
-		var searchsize = (end-start)+1;
+		let direction = null;
+		let searchsize = (end-start)+1;
 		//doing a variation of binary search by comparing range of values instead-- because the order is wrapped around, the section containing the larger value will be larger on the ends
-		//console.log("HULL SIZE",length);
+		
 		if (searchsize === 1) return 0;
 		else if (searchsize === 2) {
-			var ret0 = samept(p2,hull[0])?-999:angle3pt(p1,p2,hull[0]);
-			var ret1 = samept(p2,hull[1])?-999:angle3pt(p1,p2,hull[1]);
+			let ret0 = isSamePoint(p2,hull[0])?-999:getAngleBetween3Points(p1,p2,hull[0]);
+			let ret1 = isSamePoint(p2,hull[1])?-999:getAngleBetween3Points(p1,p2,hull[1]);
 			if(ret0 > ret1) return 0;
 			return 1;
 		}
 		while(searchsize > 2){
 			searchsize = (end-start)+1;
 
-			var s_ang = samept(p2,hull[start])?-999:angle3pt(p1,p2,hull[start]);
-			var e_ang = samept(p2,hull[end])?-999:angle3pt(p1,p2,hull[end]);
-			var split = Math.floor((searchsize)/2)+start;
-			var mid = null;
+			let s_ang = isSamePoint(p2,hull[start])?-999:getAngleBetween3Points(p1,p2,hull[start]);
+			let e_ang = isSamePoint(p2,hull[end])?-999:getAngleBetween3Points(p1,p2,hull[end]);
+			let split = Math.floor((searchsize)/2)+start;
+			let mid = null;
 			if(searchsize%2 === 0){//even case
 				leftsplit = split-1;
 				rightsplit = split;
@@ -212,14 +224,14 @@
 				rightsplit = split+1;
 			}
 
-			//console.log(start,leftsplit,mid,rightsplit,end);
-			var l_ang = samept(p2,hull[leftsplit])?-999:angle3pt(p1,p2,hull[leftsplit]);
-			var r_ang = samept(p2,hull[rightsplit])?-999:angle3pt(p1,p2,hull[rightsplit]);
-			var m_ang = mid?(samept(p2,hull[mid])?-999:angle3pt(p1,p2,hull[mid])):-9999;
-			var maxleft = Math.max(s_ang,l_ang);
-			var maxright = Math.max(r_ang,e_ang)
+			
+			let l_ang = isSamePoint(p2,hull[leftsplit])?-999:getAngleBetween3Points(p1,p2,hull[leftsplit]);
+			let r_ang = isSamePoint(p2,hull[rightsplit])?-999:getAngleBetween3Points(p1,p2,hull[rightsplit]);
+			let m_ang = mid?(isSamePoint(p2,hull[mid])?-999:getAngleBetween3Points(p1,p2,hull[mid])):-9999;
+			let maxleft = Math.max(s_ang,l_ang);
+			let maxright = Math.max(r_ang,e_ang)
 			if(m_ang >= l_ang && m_ang >= r_ang){
-				//console.log("MID");
+				
 				return mid;
 			}
 			
@@ -236,7 +248,7 @@
 
 	}
 
-	var jarvismarch = function(m,hulls){
+	function jarvisMarch(m,hulls){
 		if (hulls.length === 1) return hulls[0] //there is one hull why are we jarvismarching
 		//sort hulls based on lowest point
 		hulls.sort(function(a,b){
@@ -245,49 +257,47 @@
 		})
 		//visuals
 		ctx.strokeStyle="#FF0000";
-		for(var i = 0; i < hulls.length;i++){
-			linepts(hulls[i]);
+		for(let i = 0; i < hulls.length;i++){
+			connectPointsByLine(hulls[i]);
 		}
 		ctx.strokeStyle="#000000";
 
 		
-		var convexhull = [];
+		let convexhull = [];
 		convexhull[0] = hulls[0][0]//bottom most point;
-		var hullindex = 0;
-		var p0 = {x:0, y:convexhull[0].y};
+		let hullindex = 0;
+		let p0 = {x:0, y:convexhull[0].y};
 		//console.log("start",hulls[0][0])
 
-		for(var i = 0; i < m; i++){
-			var max_ang = -99999999;
-			var pk_1 = null;
-			var last = (i === 0)?p0:convexhull[i-1];
-			for(var j = 0; j < hulls.length; j++){
-				var res = tangent_bin_search(hulls[j],last,convexhull[i]);
+		for(let i = 0; i < m; i++){
+			let max_ang = -99999999;
+			let pk_1 = null;
+			let last = (i === 0)?p0:convexhull[i-1];
+			for(let j = 0; j < hulls.length; j++){
+				let res = tangentBinarySearch(hulls[j],last,convexhull[i]);
 				//console.log(res);
-				var ang = angle3pt(last,convexhull[i],hulls[j][res])
-				//console.log(last,convexhull[i],hulls[j][res])
-				//console.log(ang);
+				let ang = getAngleBetween3Points(last,convexhull[i],hulls[j][res])
+		
 				if(!isNaN(ang) && ang > max_ang){
 					max_ang = ang;
 					pk_1 = hulls[j][res];
 				}
 			}
-			//console.log("result",pk_1)
-			//console.log(convexhull[i])
+			
 			if(pk_1.x === convexhull[0].x && pk_1.y === convexhull[0].y) return convexhull;
 			convexhull.push(pk_1);
-			drawline(convexhull[i],pk_1);
+			renderLine(convexhull[i],pk_1);
 		}
 		return false;
 	}
 
 	//do partial hull
-	var partialhull = function(m,pts){
-		var numpartition = Math.ceil(pts.length/m);
-		var partition = [];
-		var ph_index = 0;
+	function partialHull(m,pts){
+		let numpartition = Math.ceil(pts.length/m);
+		let partition = [];
+		let ph_index = 0;
 		partition.push([]);
-		for(var i = 0; i < pts.length; i++){
+		for(let i = 0; i < pts.length; i++){
 			if(i >= (ph_index+1)*m){
 				ph_index++;
 				partition.push([]);
@@ -295,51 +305,49 @@
 			partition[ph_index].push(pts[i]);
 
 		}
-		var hulls = []
-		for(var i = 0; i < partition.length; i++){
-			var h = grahamscan(partition[i]);
+		let hulls = []
+		for(let i = 0; i < partition.length; i++){
+			let h = grahamScan(partition[i]);
 			hulls.push(h);
 		}
-		//console.log(hulls);
-		return jarvismarch(m,hulls);
-
+		
+		return jarvisMarch(m,hulls);
 
 	}
 
-	var calchull = function(){
-		var hull = null;
+	function calculateHull(){
+		let hull = null;
 		if(pts.length > 3){
-			var exp = 1;
+			let exp = 1;
 			while(!hull){
-				redrawdots();
-				var m = Math.pow(2,Math.pow(2,exp));
-				hull = partialhull(m,pts);	
+				redrawPoints(pts);
+				let m = Math.pow(2,Math.pow(2,exp));
+				hull = partialHull(m,pts);	
 				exp++;
-			}
-			//hull = partialhull(16);	
+			}			
 		}
 		else 
 			hull = pts;
-		linepts(hull);
+		connectPointsByLine(hull);
 
 	}
 
 	$("#gen_pts").on("click",function(){
-		generatedots();
+		generatePoints();
 	})
 	$("#gen_hull").on("click",function(){
-		calchull();
+		calculateHull();
 	})
 	$("#clear").on("click",function(){
 		clearCanvas();
 		pts = [];
 	})
 	$(canvas).on("click",function(e){
-    	var x = e.pageX - offset.left;
-    	var y = e.pageY - offset.top;
-		var dot = {x:x,y:y};
-		drawdot(dot);
+    	let x = e.pageX - canvasOffset.left;
+    	let y = e.pageY - canvasOffset.top;
+		let dot = {x:x,y:y};
+		renderPoint(dot);
 		pts.push(dot);
 	})
-}();
+})();
 
